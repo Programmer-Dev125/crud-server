448b58dfb0fe1caff12da78f6a5a9bbd0f4604fc
Programmer-Dev125 <programmerdev125@gmail.com>
2025-03-11 12:07:12 -0500

    ip

--- app.js
+++ app.js
@@ -1,15 +1,18 @@
 import mongoose, { Schema } from "mongoose";
 import http from "node:http";
 
-const port = process.env.PORT;
-const host = process.env.HOST;
+// const port = process.env.PORT;
+// const host = process.env.HOST;
 const URL = process.env.MONGO_URL;
 
 const conn = mongoose.createConnection(URL);
-const model = conn.model("customModel", new Schema({id: Number, name: String}), "user");
+const model = conn.model(
+  "customModel",
+  new Schema({ id: Number, name: String }),
+  "user"
+);
 
-
-const server = http.createServer(await (req, res) => {
+const server = http.createServer(async (req, res) => {
   res.setHeader("access-control-allow-origin", "*");
   res.setHeader("access-control-allow-methods", "GET, POST, DELETE, PUT");
   res.setHeader("access-control-allow-headers", "content-type, x-user-id");
@@ -22,14 +25,14 @@ const server = http.createServer(await (req, res) => {
   }
 
   if (req.method === "GET") {
-   const isData = await model.find({}, {_id: 0, __v: 0});
-   if(Array.isArray(isData)){
-        res.writeHead(200);
-        res.end(JSON.stringify(isData));
-   }else{
-    res.writeHead(500);
-    res.end(JSON.stringify({error: "Error fetching data"}));
-   }
+    const isData = await model.find({}, { _id: 0, __v: 0 });
+    if (Array.isArray(isData)) {
+      res.writeHead(200);
+      res.end(JSON.stringify(isData));
+    } else {
+      res.writeHead(500);
+      res.end(JSON.stringify({ error: "Error fetching data" }));
+    }
   } else {
     res.writeHead(405);
     res.end(
