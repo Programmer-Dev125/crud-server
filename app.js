9d8443419b92fabb9c81b7032b4adf6649067a8b
Programmer-Dev125 <programmerdev125@gmail.com>
2025-03-10 19:09:44 -0500

    Add POST

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+import { error } from "node:console";
 import { createServer } from "node:http";
 
 const PORT = process.env.PORT || 3000;
@@ -12,10 +13,31 @@ const data = [
 const server = createServer((req, res) => {
   res.setHeader("access-control-allow-origin", "*");
   res.setHeader("content-type", "application/json");
+  res.setHeader("access-control-allow-headers", "content-type");
 
   if (req.method === "GET") {
     res.writeHead(200);
     res.end(JSON.stringify(data));
+  } else if (req.method === "POST") {
+    let isBody;
+    req.on("data", (data) => {
+      isBody = JSON.stringify(data.toString());
+    });
+    req.on("end", () => {
+      const isObj = { id: data.length + 1, name: isBody.name };
+      const ln = data.length;
+      data.push(isObj);
+      if (ln < data.length) {
+        res.writeHead(200);
+        res.end(JSON.stringify(data));
+      } else {
+        res.writeHead(400);
+        res.end(JSON.stringify({ error: "Failed to submit user" }));
+      }
+    });
+  } else {
+    res.writeHead(405);
+    res.end(JSON.stringify({ error: "Method not supported" }));
   }
 });
 
